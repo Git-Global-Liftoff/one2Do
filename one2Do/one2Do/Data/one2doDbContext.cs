a876e75ce6bba1890a0666764679ecee0bb8c0c9
e-bold <enkhmunkh@att.net>
2024-07-01 13:19:19 -0500

    Setup Scaffolding and Identity interface

---
+++ one2Do/one2Do/Data/one2doDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace one2Do;
+
+public class one2doDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
+{
+    public one2doDbContext(DbContextOptions<one2doDbContext> options) : base(options)
+        {
+        }
+}

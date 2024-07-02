c0c0e5837f1099a4033be5d52f97ed817bb33691
Hannah Overton <hannahoverton99@gmail.com>
2024-07-02 10:58:00 -0500

    hannah-created ListTemplate MVC

---
+++ one2Do/one2Do/Controllers/ListTemplate.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace one2Do.Controllers;
+
+public class ListTemplateController : Controller
+{
+    private one2doDbContext context;
+
+    public ListTemplateController(one2doDbContext dbContext)
+    {
+        context= dbContext;
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ one2Do/one2Do/Models/ListTemplate.cs
@@ -0,0 +1,22 @@
+﻿namespace one2Do.Models;
+
+public class ListTemplate
+{
+    public int Id {get; set; }
+    public string? Title {get; set; }
+    public List<Task>? Tasks {get; set; }
+    public List<Categories>? Categories {get; set; }
+
+    public ListTemplate(string title, int id)
+    {
+        Title = title;
+        Id= id;
+        Tasks = new List<Task>();
+    }
+    public ListTemplate()
+    {
+
+    }
+
+
+}

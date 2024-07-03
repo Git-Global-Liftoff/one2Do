a6c8b72e6d556cd4a9d2e8029f6ccb5823e9b253
mendulla <mendulla@yahoo.com>
2024-07-03 14:58:03 -0500

    Created ToDo Models folder with ToDoList and TaskItem files. Then Updated DbContext

--- one2Do/one2Do/Data/one2doDbContext.cs
+++ one2Do/one2Do/Data/one2doDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using one2Do.Models;
 using one2Do.Models.QuoteModels; // Add namespace for Quote model
+using one2Do.Models.ToDoModels; //Add namespace for ToDoModels
 
 namespace one2Do
 {
@@ -14,6 +15,8 @@ namespace one2Do
         }
 
         // DbSet property for Quotes table
-        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Quote> Quotes { get; set; } //Add DbSet for Quote
+        public DbSet<ToDoList> ToDoLists { get; set; } // Add DbSet for ToDoList
+        public DbSet<TaskItem> TaskItems { get; set; } // Add DbSet for TaskItem
     }
 }

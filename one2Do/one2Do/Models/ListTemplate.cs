0210c61d62d7749d558a3d547dd489d6f26d5afa
Hannah Overton <hannahoverton99@gmail.com>
2024-07-04 20:30:45 -0500

    Changed List<Task> Tasks to List<TaskItem> TaskItems in ListTemplate.cs

--- one2Do/one2Do/Models/ListTemplate.cs
+++ one2Do/one2Do/Models/ListTemplate.cs
@@ -1,17 +1,19 @@
-﻿namespace one2Do.Models;
+﻿using one2Do.Models.ToDoModels;
+
+namespace one2Do.Models;
 
 public class ListTemplate
 {
     public int Id {get; set; }
     public string? Title {get; set; }
-    public List<Task>? Tasks {get; set; }
+    public List<TaskItem> TaskItems {get; set; }
     public List<Categories>? Categories {get; set; }
 
     public ListTemplate(string title, int id)
     {
         Title = title;
         Id= id;
-        Tasks = new List<Task>();
+        TaskItems = new List<TaskItem>();
     }
     public ListTemplate()
     {

61887591e5a3c2219084ac42c66545224f54a134
Katie Nelson <katienelson@Katies-Air-2.attlocal.net>
2024-07-10 15:37:15 -0500

    connected HouseholdToDo List button to saved list

--- one2Do/one2Do/Controllers/HouseholdToDoListController.cs
+++ one2Do/one2Do/Controllers/HouseholdToDoListController.cs
@@ -1,80 +1,98 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using one2Do.Controllers;
 using one2Do.Data;
 using one2Do.Models.ToDoModels;
-using System.Security.Claims;
-
 
 namespace one2Do;
 
-
-    [Authorize] 
-
-    public class HouseholdToDoController : Controller
+[Authorize]
+public class HouseholdToDoListController : Controller
+{
+    // Define household items as properties
+    private readonly one2doDbContext _context;
+    private readonly string[] HouseholdItems =
     {
-        // Define household items as properties
-       private readonly one2doDbContext _context;
-        private readonly string[] HouseholdItems = { "Laundry", "Organize Closets", "Iron", "Dishes", "Wash Baseboards", "Dust", "Change Linens" };
+        "Laundry",
+        "Organize Closets",
+        "Iron",
+        "Dishes",
+        "Wash Baseboards",
+        "Dust",
+        "Change Linens"
+    };
 
-        public HouseholdToDoController(one2doDbContext context) 
-        {
-            _context = context;
-        }
-
-        // You can override methods from ToDoListController if necessary
-
-        // GET: HouseholdToDoList/Create
-        // //public IActionResult Create()
-        // {
-
-        //     var viewModel = new AddToDoListViewModel
-        //     {
-        //         Categories = _context.Categories.Select(c => new SelectListItem
-        //         {
-        //             Value = c.Id.ToString(),
-        //             Text = c.Name
-        //         }).ToList(),
-        //         HouseholdItems = HouseholdItems // Pass household items to the view model
-        //     };
-        //     return View(viewModel);
-        // }
-
-        // POST: HouseholdToDoList/Create
-        [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public IActionResult Create()
-        {
-        
-            var theCategory = _context.Categories.Find(1);
-                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // More reliable way to get user ID
-                // var newToDoList = new ToDoList
-                // {
-                //     Title = "Household ToDo Template",
-                //     UserId = userId,
-                //     CategoryId = 1 // Store only the CategoryId
-                // };
-                var newToDoList = new ToDoList(
-                    "Household ToDo Template",
-                    userId,
-                    1,
-                    theCategory,
-                    "To do around the house",
-                    DateTime.Now, 
-                    false
-
-                );
-
-                _context.Add(newToDoList);
-                 _context.SaveChanges();
-                //return RedirectToAction(nameof(Index));
-                return Redirect("/");
-            
-           
-        }
-
+    public HouseholdToDoListController(one2doDbContext context)
+    {
+        _context = context;
     }
+
+    // You can override methods from ToDoListController if necessary
+
+    // GET: HouseholdToDoList/Create
+    // //public IActionResult Create()
+    // {
+
+    //     var viewModel = new AddToDoListViewModel
+    //     {
+    //         Categories = _context.Categories.Select(c => new SelectListItem
+    //         {
+    //             Value = c.Id.ToString(),
+    //             Text = c.Name
+    //         }).ToList(),
+    //         HouseholdItems = HouseholdItems // Pass household items to the view model
+    //     };
+    //     return View(viewModel);
+    // }
+
+    // POST: HouseholdToDoList/Create
+    [HttpPost]
+    //[ValidateAntiForgeryToken]
+    public IActionResult Create()
+    {
+        
+        var theCategory = _context.Categories.Find(1);
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier); // More reliable way to get user ID
+        // var newToDoList = new ToDoList
+        // {
+        //     Title = "Household ToDo Template",
+        //     UserId = userId,
+        //     CategoryId = 1 // Store only the CategoryId
+        // };
+        var newToDoList = new ToDoList(
+            "Household ToDo Template",
+            userId,
+            1,
+            theCategory,
+            "To do around the house",
+            DateTime.Now,
+            false
+        );
+
+        foreach (var item in HouseholdItems)
+        {
+            var taskItem = new TaskItem
+            {
+                Description = item,
+                DueDate = DateTime.Now, 
+                IsCompleted = false,
+                ToDoList = newToDoList,
+                ToDoListId = newToDoList.Id
+            };
+            _context.TaskItems.Add(taskItem);
+        }
+
+        _context.Add(newToDoList);
+        _context.SaveChanges();
+
+        //return RedirectToAction(nameof(Index));
+        return RedirectToAction("Index", "ToDoList"); // Redirect to the appropriate action and controller
+    }
+}
+
+
 // {
 //             Title = title;
 //             UserId = userId;
@@ -84,3 +102,5 @@ namespace one2Do;
 //             DueDate = dueDate;
 //             IsCompleted = isCompleted;
 //         }
+
+

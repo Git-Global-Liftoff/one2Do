623632c370bda63fa8f47e28f9e22417a605c3d4
Hannah Overton <hannahoverton99@gmail.com>
2024-07-11 16:45:00 -0500

    added styling to nav buttons and updated progress tracker to appear on individual lists

--- one2Do/one2Do/ViewModels/ToDoListItemViewModel.cs
+++ one2Do/one2Do/ViewModels/ToDoListItemViewModel.cs
@@ -8,5 +8,8 @@ namespace one2Do.ViewModels
         public string Title { get; set; } = string.Empty;
         public List<TaskItemViewModel> TaskItems { get; set; } = new List<TaskItemViewModel>();
         public string CategoryName { get; set; } = string.Empty;
+        public int TotalTasks { get; set;}
+        public int CompletedTasks {get; set;}
     }
+
 }
